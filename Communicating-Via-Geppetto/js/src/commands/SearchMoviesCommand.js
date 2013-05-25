9e49ff0596ba407312457521f476f5fbeb16899b
Nikhilesh Katakam <nikhilesh.k@gmail.com>
2013-05-24 20:59:54 -0700

    example2 upload

---
+++ Communicating-Via-Geppetto/js/src/commands/SearchMoviesCommand.js
@@ -0,0 +1,34 @@
+define(["jquery","underscore"], function($,_) {
+	
+
+	var command = function() {
+	};
+
+	command.prototype.execute = function() {
+		_.bindAll(this);
+		console.log(this.eventData);
+		var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=78ejsdd76tc6jsffmrxjddxu";
+		url += "&q=" + this.eventData.data.get("title");
+		url += "&page_limit=1";
+		var that = this;
+		$.ajax({
+			url : url,
+			success : function(data) {
+				that.handleDataLoadSuccess(data);
+			},
+			error : function(e) {
+				that.handleDataLoadError(e);
+			}
+		})
+	}; 
+	
+	command.prototype.handleDataLoadSuccess = function(data){
+		console.log(data);
+	};
+	
+	command.prototype.handleDataLoadError = function(e){
+		console.log(e);
+	};
+
+	return command;
+})
