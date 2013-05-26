57f842342d992ad544e40b78e9fb94942e44e662
Nikhilesh Katakam <nikhilesh.k@gmail.com>
2013-05-26 12:10:33 -0700

    Adding more comments

--- Communicating-Via-Geppetto/js/src/controller/ApplicationContext.js
+++ Communicating-Via-Geppetto/js/src/controller/ApplicationContext.js
@@ -7,9 +7,13 @@ function(
 	Geppetto, 
 	SearchMoviesCommand) {
 
+	//return a geppetto context
 	return Geppetto.Context.extend({
+		//setup an initialize function
 		initialize : function() {
-			// map commands here...
+			// map commands 
+			//when ever a "performSearchEvent" is dispatch on this command
+			//the context delegates that call to the SearchMoviesCommand
 			this.mapCommand( "performSearchEvent"/*event name*/, SearchMoviesCommand );
 		}
 	});
